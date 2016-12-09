1be58e2faa990775154d64343ced6a782309d238
marleysid <bsiddhartha25@gmail.com>
2016-12-09 14:44:18 +0545

    added dispatcher and actions

--- src/js/pages/Todos.js
+++ src/js/pages/Todos.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
 import TodoStore from "../stores/TodoStore";
 
 export default class Todos extends React.Component{
@@ -8,23 +9,46 @@ export default class Todos extends React.Component{
 		super();
 		this.state = {
 			todos: TodoStore.getAll(),
-		};
-		
+		};		
+	}
+
+
+	//fires only first time. eventlistener
+	componentWillMount() {
+		TodoStore.on("change", () => {
+			this.setState({
+				todos: TodoStore.getAll()
+			});	
+		});
+	}
+
+
+	createTodo(){
+	    //input
+	    const todoTitle = this.state.mytodo;
+		TodoActions.createTodo(todoTitle);
+	}
+
+
+	handleChange(e){
+		// const todo = e.target.value;
+		// console.log(todo);
+		this.setState({ mytodo: e.target.value });
 	}
 
 
 	render(){
-
 		const { todos } = this.state;
-
 		const TodoComponents = todos.map((todo) => {
 			return <Todo key={todo.id} {...todo}/>;
 		});
 
 		return(
 		    <div>
+		      <button onClick={this.createTodo.bind(this)}>Create Todo</button>
+		      <input onChange={this.handleChange.bind(this)}/>
 		       <h4>List Of Todos</h4>
-				<ul>{TodoComponents}</ul>
+			   <ul>{TodoComponents}</ul>
 			</div>
 			);
 	}
--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -6,6 +6,8 @@
 */
 import { EventEmitter } from "events";
 
+import dispatcher from "../dispatcher";
+
 class TodoStore extends EventEmitter{
 	constructor() {
 		super();
@@ -29,11 +31,37 @@ class TodoStore extends EventEmitter{
 
 	}
 
-		getAll(){
-			return this.todos;
+
+	createTodo(text) {
+		const id = Date.now();
+		this.todos.push({
+			id,
+			text,
+			complete: false,
+		});
+
+		this.emit("change");
+	}
+
+	getAll(){
+		return this.todos;
+	}
+
+
+	handleActions(action) {
+		//console.log("TodoStore received an action", action);
+		switch(action.type){
+			case "CREATE_TODO":{
+				this.createTodo(action.text);
+			}
 		}
+	}
+
+
 }
 
 const todoStore = new TodoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore))
+//window.dispatcher = dispatcher;
 
 export default todoStore;
